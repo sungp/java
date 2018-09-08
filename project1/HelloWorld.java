91ee885a29234fb6440e4b61fc8632e7caec2151
Ryan Park <rjpark2004@gmail.com>
2018-09-07 20:45:45 -0700

    oOFOOFOofofofofofOOOFOFOFfoOFOFOoF

--- project1/HelloWorld.java
+++ project1/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("reeeeeeeeeeee");
+        System.out.println("ooooooooooof");
     }
 
 }
